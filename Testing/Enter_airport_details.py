3a5a8bebe07a1451f09bcf7240f177becbb2998b
QuantumGamer169 <68508529+QuantumGamer169@users.noreply.github.com>
2020-10-08 10:57:49 +0100

    Update Enter_airport_details.py

--- Testing/Enter_airport_details.py
+++ Testing/Enter_airport_details.py
@@ -3,7 +3,14 @@ UK_Airport = (UK_Airport.upper())
 OverSeas_Airport = str(input("Please enter a 3 letter idetifier for overseas airport:    "))
 OverSeas_Airport = (OverSeas_Airport.upper())
 file = open("Airports.txt", "r")
-for i in range (5):
+if UK_Airport = ("LPL"):
+  for i in range (5):
+  line = file.readline()
+  data = line.split(",")
+  if  data[0] == OverSeas_Airport:
+    print (data[0:4])
+elif UK_Airport = ("BOH"):
+  for i in range (5):
   line = file.readline()
   data = line.split(",")
   if  data[0] == OverSeas_Airport:
